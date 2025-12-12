ad7d27925ff876dac9718c6f05a767cc7d5afd45
Pauline <cpaulinecapulong11617@gmail.com>
2025-12-12 16:19:18 +0800

    Update sidebar.tsx

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -27,7 +27,7 @@ const Sidebar: React.FC<SidebarProps> = ({ activePage = "", setActivePage }) => 
   return (
     <div className="sidebar">
       <div>
-        <img src="/src/assets/images.png" alt="App Logo" className="sidebar-logo" />
+        <img src="../src/assets/images.png" alt="App Logo" className="sidebar-logo" />
         <h3>SPC Medical</h3>
 
         <ul className="sidebar-menu">
@@ -92,4 +92,5 @@ const Sidebar: React.FC<SidebarProps> = ({ activePage = "", setActivePage }) => 
 };
 
 export default Sidebar;
+
 
